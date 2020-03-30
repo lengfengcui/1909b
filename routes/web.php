9072cd22c2801aa09e9ee056c2fc7356c0a1ae2c
liu <2938937056@qq.com>
2020-03-30 13:57:50 +0800

    1909b

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::prefix('client')->group(function(){
+Route::get('create', 'ClientController@create');
+Route::post('store', 'ClientController@store');
+Route::get('index', 'ClientController@index');
+Route::get('destroy', 'ClientController@destroy');
+Route::get('edit/{id}', 'ClientController@edit');
+Route::post('update/{id}', 'ClientController@update');
+Route::get('ajaxjdjg', 'ClientController@ajaxjdjg');
 });
